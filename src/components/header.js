dc5561df5c68e226951912be97dbec4122bf6043
abbas <abbasalkabbi2003@gmail.com>
2023-02-02 02:45:55 +0300

    Edit => Header

--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,11 @@ import './header.css'
 import logo from "../images/youtube-logo.png"
 function Header(){
     return(
-        <header>
-            <div class="left-section">
-                <i class="material-icons logo">menu</i>
-                <img src={logo}  alt='ds'/>
-            </div>
-            <div class="mid-section">
-                <form action="">
-                    <input type="text" placeholder="Search"/>
-                    <button><i class='material-icons'>search</i></button>
-                </form>
-                <i class="material-icons mic">mic</i>
-            </div>
-            <div class="right-section">
-                <a href="h.html"><i class='material-icons'>video_call</i></a>
-                <a href="h.html"><i class='material-icons'>apps</i></a>
-                <a href="h.html"><i class='material-icons'>notifications</i></a>
-                <a href="h.html"><i class='material-icons'>account_circle</i></a>
-            </div>
-        </header>
+        <>
+        <h1>
+            Header
+        </h1>
+        </>
     )
 }
 export default Header;
