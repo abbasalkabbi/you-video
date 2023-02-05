89725633dcb0636c4d12091ba66271c2c203056f
abbas <abbasalkabbi2003@gmail.com>
2023-02-06 02:18:05 +0300

    Edit =>video

--- src/pages/video.js
+++ src/pages/video.js
@@ -30,12 +30,49 @@ class Video extends Component{
     // video
     video(){
         if(this.state.finished === true){
+            let {id_video,id_author,name_video,url_video,url_img,date_video,views}=this.state.video[0];
             return(
                 <>
-                <h1>
-                    {this.state.video[0].id_video} 
-                    {this.state.video[0].name_video} 
-                </h1>
+ {/* video */}
+                <div className="video  col-sm-12 col-md-9 ">
+                        {/* video play */}
+                        <div class="ratio ratio-16x9">
+                                <video width="320" height="240" controls>
+                                    <source src={require(`../assets/${url_video}`)} type="video/mp4"/>
+                                </video>
+                        </div>
+                         {/* video play */}
+                         {/* card body */}
+                        <div className="card">
+                            {/* title */}
+                            <div className="card-header row">
+                                <h5 className="card-title col-8">{name_video}</h5>
+                                <p className="info-count card-text col-4">{views} viwes 10 likes 20 unlike</p>
+                            </div>
+                            {/* end title */}
+                            {/* card body */}
+                            <div className="card-body row"></div>
+                            {/* like list */}
+                            <div className="btn-group col-4 like-unlike" role="group" aria-label="Basic example">
+                                    <button type="button" className="btn btn-outline-primary"> like</button>
+                                    <button type="button" className="btn btn-outline-danger">unlike</button>
+                            </div>
+                            {/*End  like list */}
+                            {/* channel */}
+                            <a className="col-8 channel " href="/l">
+                                    <p className="">
+                                        Abbas Alkbbi
+                                    </p>
+                                    <img src={require('../assets/avatarfull_original.png')} class="channel-img  img-thumbnail " alt="..." style={{'width':'100px'}}/>
+                                </a>
+                            {/*END channel */}
+                            </div>
+                            {/* card body End */}
+                        </div>
+                        {/* end card */}
+                        
+                    </div>
+                    {/* video End*/}
                 </>
             )
         }
@@ -54,9 +91,17 @@ class Video extends Component{
         return(
             <>
             <Header/>
-            {/* video */}
-            
-            {/* video End */}
+            {/* container*/}
+            <div className="container">
+                {/* div for video and list  */}
+                <div className="row">
+                    {/* video */}
+                    {this.video()}
+                    {/* video End */}
+                </div>
+                {/* div for video and list  End*/}
+            </div>
+            {/* container End */}
             </>
         )
     }
